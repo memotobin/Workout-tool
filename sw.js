688c64401bb294a2b664eda27282c39343d08a4f
memotobin <memotobin@gmail.com>
2026-04-25 18:52:50 -0700

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'ppl-tracker-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE_NAME = 'ppl-tracker-v2';
+const ASSETS = ['/', '/Workout-tool/', '/Workout-tool/index.html', '/Workout-tool/manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS).catch(() => {}))
   );
   self.skipWaiting();
 });
@@ -19,6 +19,6 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((r) => r || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
